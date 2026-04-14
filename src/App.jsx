efa4da49825c8f92108a125debec24b5d219c265
vbar <vobarkal@gmail.com>
2026-04-14 21:31:02 +0200

    home page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import { LoadingScreen } from './components/LoadingScreen';
 import { Navbar } from './components/Navbar';
 import { MobileMenu } from './components/MobileMenu';
+import { Home } from './components/sections/Home';
+import { About } from './components/sections/About';
 import "./index.css";
 import { useState } from 'react';
 
@@ -15,6 +17,8 @@ function App() {
          bg-black text-gray-100`}>
           <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
           <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+          <Home />
+          <About />
         </div>
     </>
   )
---
+++ src/components/sections/About.jsx
@@ -0,0 +1,14 @@
+export const About = () => {
+  return (
+    <section
+      id="about"
+      className="min-h-screen flex items-center justify-center relative"
+    >
+      <div className="text-center z-10 px-4">
+        <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">
+          This is about page
+        </h1>
+      </div>
+    </section>
+  );
+};
