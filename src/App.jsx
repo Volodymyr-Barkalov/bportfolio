6297d51517c36a7cc6015056b87f35218880322f
vbar <vobarkal@gmail.com>
2026-04-23 19:07:12 +0200

    added Contact component, configs

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { About } from './components/sections/About';
 import { Projects } from './components/sections/Projects';
 import "./index.css";
 import { useState } from 'react';
+import { Contact } from './components/sections/Contact';
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -21,6 +22,7 @@ function App() {
           <Home />
           <About />
           <Projects />
+          <Contact />
         </div>
     </>
   )
